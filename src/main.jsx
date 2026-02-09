ac1ed156e13e217d0486144fd248ee4b2d202dc2
m248m <drs.tky@gmail.com>
2026-02-09 22:53:59 +0900

    ver1.1

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// ★追加1: Amplifyライブラリの読み込み
+import { Amplify } from 'aws-amplify';
+
+// ★追加2: Cognitoの設定
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: 'ap-northeast-1_gRJQ6gpSb', // ここを書き換える
+      userPoolClientId: '2nft4ghqsg26vgojl8jd92vo8o',    // ここを書き換える
+    }
+  }
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
